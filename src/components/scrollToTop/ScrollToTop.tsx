898ed05c0fe14d0f9a9c3d24edd645167c29226e
Sihab Hossain <sihabhossain380@gmail.com>
2024-08-31 22:36:29 +0600

    deployed

--- src/components/scrollToTop/ScrollToTop.tsx
+++ src/components/scrollToTop/ScrollToTop.tsx
@@ -24,30 +24,28 @@ const ScrollToTopButton: React.FC = () => {
     });
   };
 
-  return (
-    isVisible && (
-      <div
-        className="fixed bottom-6 right-6 flex w-[200px] cursor-pointer items-center justify-center space-x-2 rounded-full bg-primary-500 p-3 text-white shadow-lg"
-        style={{ left: "50%", transform: "translateX(-50%)" }}
-        onClick={scrollToTop}
-        aria-label="Scroll to Top"
+  return isVisible ? (
+    <div
+      className="fixed bottom-6 right-6 flex w-[200px] cursor-pointer items-center justify-center space-x-2 rounded-full bg-primary-500 p-3 text-white shadow-lg"
+      style={{ left: "50%", transform: "translateX(-50%)" }}
+      onClick={scrollToTop}
+      aria-label="Scroll to Top"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M19 15l-7-7-7 7" />
-        </svg>
-        <span>Scroll to Top</span>
-      </div>
-    )
-  );
+        <path d="M19 15l-7-7-7 7" />
+      </svg>
+      <span>Scroll to Top</span>
+    </div>
+  ) : null;
 };
 
 export default ScrollToTopButton;
